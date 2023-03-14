40e3949e2b4b8d31b10a1ebdba4c132f88ab51e9
Vinkario <gadblake66@gmail.com>
2023-03-14 11:20:13 -0300

    postitions correct

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,24 +6,23 @@ const closeButton = document.querySelector('.close')
 function moverBotao() {
     const windowHeight = window.innerHeight;
     const windowWidth = window.innerWidth;
-  
-    // Define novas coordenadas aleatórias para o botão
+
     const newTop = Math.floor(Math.random() * (windowHeight - simButton.offsetHeight));
     const newLeft = Math.floor(Math.random() * (windowWidth - simButton.offsetWidth));
-  
-    // Move o botão para as novas coordenadas
-    simButton.style.marginTop = newTop + 'px';
-    simButton.style.marginLeft = newLeft + 'px';
+
+    simButton.style.top = newTop + 'px';
+    simButton.style.left = newLeft + 'px';
 }
 
-function modal(){
-    modalOn.style.display='flex'
+function modal() {
+    modalOn.style.display = 'flex'
+
 }
 
-function fechar(){
-    modalOn.style.display='none'
+function fechar() {
+    modalOn.style.display = 'none'
 }
 
 simButton.addEventListener("click", moverBotao)
-naoButton.addEventListener('click',modal)
-closeButton.addEventListener('click',fechar)
+naoButton.addEventListener('click', modal)
+closeButton.addEventListener('click', fechar)
